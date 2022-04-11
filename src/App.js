e84566ba387c83ab6e3826247edbd979b398c4a6
donovanel <donovan.el@northeastern.edu>
2022-04-11 10:07:49 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import MoviePage from "./components/movie-page";
+
+function App() {
+  return (
+      <div className="container">
+        <MoviePage/>
+      </div>
+  );
+}
+
+export default App;
