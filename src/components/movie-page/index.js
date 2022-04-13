0979f00368b0f1300b0c1aa51c3f2f69aa3bd290
donovanel <donovan.el@northeastern.edu>
2022-04-13 09:11:48 -0400

    add homepage to json, redirect for more info

--- src/components/movie-page/index.js
+++ src/components/movie-page/index.js
@@ -8,6 +8,7 @@ const MoviePage = (
             "title":"Fight Club",
             "release_date":"1999-10-15",
             "overview":"A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground fight clubs forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
+            "homepage":"http://www.foxmovies.com/movies/fight-club",
             "poster_path":"/images/fight-club.jpg",
             "genres":[{"id":18,"name":"Drama"}],
             "vote_average":8.4
@@ -25,6 +26,7 @@ const MoviePage = (
                     <span>{movie.genres[0].name}</span> | <span>{movie.release_date}</span>
                     <h3>Overview</h3>
                     <p>{movie.overview}</p>
+                    <a href={movie.homepage}>More info</a>
                 </div>
             </div>
 
