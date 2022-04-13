e72903fba9bbc34d082c07642716ff9c6d4aafb4
donovanel <donovan.el@northeastern.edu>
2022-04-13 10:28:54 -0400

    review button

--- src/components/movie-page/index.js
+++ src/components/movie-page/index.js
@@ -9,7 +9,7 @@ const MoviePage = (
             "release_date":"1999-10-15",
             "overview":"A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground fight clubs forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
             "homepage":"http://www.foxmovies.com/movies/fight-club",
-            "poster_path":"/images/fight-club.jpg",
+            "poster_path":"../../images/fight-club.jpg",
             "genres":[{"id":18,"name":"Drama"}],
             "vote_average":8.4
         }
@@ -17,20 +17,24 @@ const MoviePage = (
     return(
         <>
             <div className="row">
-                <div className="col">
+                <div className="col-4">
                     <img src={movie.poster_path} width="50"/>
+                    <div className="row">
+                        <button>Write Review</button>
+                    </div>
                 </div>
 
-                <div className="col">
+                <div className="col-8">
                     <h1>{movie.title}</h1>
                     <span>{movie.genres[0].name}</span> | <span>{movie.release_date}</span>
                     <h3>Overview</h3>
                     <p>{movie.overview}</p>
                     <a href={movie.homepage}>More info</a>
+
+                    <h2>Reviews</h2>
+
                 </div>
             </div>
-
-
         </>
     )
 };
