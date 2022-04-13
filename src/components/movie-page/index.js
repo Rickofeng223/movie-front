203ea59bdd92dcd242b4f239455933ba8bd8ef20
donovanel <donovan.el@northeastern.edu>
2022-04-13 13:46:57 -0400

    set columns

--- src/components/movie-page/index.js
+++ src/components/movie-page/index.js
@@ -18,14 +18,14 @@ const MoviePage = (
     return(
         <>
             <div className="row">
-                <div className="col">
+                <div className="col-2">
                     <img src={movie.poster_path} width="50"/>
                     <div className="row">
                         <button>Write Review</button>
                     </div>
                 </div>
 
-                <div className="col">
+                <div className="col-8">
                     <h1>{movie.title}</h1>
                     <span>{movie.genres[0].name}</span> | <span>{movie.release_date}</span>
                     <h3>Overview</h3>
