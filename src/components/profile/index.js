984a67933111048e676de067743eeb245cb14f0a
donovanel <donovan.el@northeastern.edu>
2022-04-25 22:24:21 -0400

    resolve

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import React, {useState} from "react";
-import {useSelector, useDispatch} from "react-redux";
-
-const ProfileComponent = () => {
-    const user = useSelector((state) => state.profile);
-
-    return(
-        <>
-            <div className="card">
-                <img className="card-img-top" src="/public/images/background.jpg"
-                     alt="default popcorn background"
-                    height="200px"
-                    width="100%"/>
-                <div className="card-body">
-                    <h2 className="card-title">{user.first_name} {user.last_name}</h2>
-                    <h3>{user.username}</h3>
-
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default ProfileComponent;
-=======
 import React from "react";
 import profile from "../data/user.json";
 
