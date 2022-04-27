e4e5b333c06a891e08bf1d8501fe137105016a12
donovanel <donovan.el@northeastern.edu>
2022-04-27 12:44:56 -0400

    profile image

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -20,10 +20,14 @@ const ProfileComponent = () => {
       <div className="card">
           <img className="card-img-top" src="/images/background.jpg" alt="popcorn banner"/>
         <div className="card-body">
+          <img className="img-thumbnail rounded-circle" src="/images/user-placeholder.png" alt="person silhouette" width="150"/>
           <h2 className="card-title">
               {user.first_name} {user.last_name}
           </h2>
-           <h3><small className="text-muted">@{user.username}</small></h3>
+           <h3>
+               <small className="text-muted">@{user.username}</small>
+           </h3>
+
         </div>
       </div>
     </>
