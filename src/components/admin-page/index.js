cf6e07f44c14d60b5794e41d7b4bc28da051b1d0
sam raphael <samraphael@gmail.com>
2022-04-28 19:44:43 -0400

    admin page only shows if role is admin... displays userlist / reviewlist

--- src/components/admin-page/index.js
+++ src/components/admin-page/index.js
@@ -1,25 +1,31 @@
 import React from "react";
-import {Tabs, Tab} from "react-bootstrap";
+import {Tab, Tabs} from "react-bootstrap";
 
 import ReviewList from "../review-list";
 import UserList from "../user-list";
+import {useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
 
 const AdminPage = () => {
-
+    const user = useSelector(e => e.currentUser)
+    const navigate = useNavigate()
     return (
-        <>
+        <>{user && user.role === "ADMIN" ? (<>
             <h1 className="mt-4 mb-3">Manage Site</h1>
 
             <Tabs defaultActiveKey="users" id="admin-tabs" className="mb-3">
                 <Tab eventKey="users" title="Users">
-                    <UserList/>
+                    <UserList uid={user._id}/>
                 </Tab>
                 <Tab eventKey="reviews" title="Reviews">
                     <ReviewList/>
                 </Tab>
             </Tabs>
+
+            </>) : navigate('/')}
         </>
     )
 };
 
 export default AdminPage;
+
