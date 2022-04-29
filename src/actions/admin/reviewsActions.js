59ac11d7d0d3ce1ae6148b6aa297aae349a0d4c4
sam raphael <samraphael@gmail.com>
2022-04-29 16:21:49 -0400

    update user/review actions

--- src/actions/admin/reviewsActions.js
+++ src/actions/admin/reviewsActions.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const getReviews = async (userid,review,dispatch)=>{
+export const getReviews = async (userid,review,dispatch,sortType='recent')=>{
     try{
-        const {data:reviews} = await axios.get(`http://localhost:4000/api/reviews/?user=${userid}`)
-        dispatch({type: 'get-reviews', reviews})
+        const {data:reviews} = await axios.get(`http://localhost:4000/api/reviews/?user=${userid}&sort=${sortType}`)
+        dispatch({type: 'get-reviews', reviews:reviews})
     }catch (e) {
         console.log(e)
         alert(`ERROR: ${e.message}`)
@@ -11,7 +11,10 @@ export const getReviews = async (userid,review,dispatch)=>{
 }
 export const deleteReview = async (userid,review,dispatch)=>{
     try{
-        const response = await axios.delete(`http://localhost:4000/api/reviews/?user=${userid}`)
+
+        const response = await axios.delete(
+            `http://localhost:4000/api/reviews/${review._id}?user=${userid}`
+        )
         dispatch({type: 'delete-review', review})
     }catch (e) {
         console.log(e)
--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,36 +1,48 @@
 import axios from "axios";
-export const LOGIN='login', UPDATE='update',LOGOUT='logout',PROFILE='profile'
 
-export const signup = async (user_data,{navigate,dispatch}) => {
+export const LOGIN = 'login', UPDATE = 'update-profile', LOGOUT = 'logout', PROFILE = 'profile'
+
+
+export const signup = async (user_data, {navigate, dispatch}) => {
     try {
-        let {data} = await axios.post(`http://localhost:4000/api/auth/signup`,user_data)
-        dispatch({type:LOGIN, user:data})
+        let {data} = await axios.post(`http://localhost:4000/api/auth/signup`, user_data)
+       let _=await axios.get(`http://localhost:4000/api/session/get`)
+        dispatch({type: LOGIN, user: data})
+        navigate('/')
+    } catch (e) {
+        navigate('/signup-error')
+        console.log(e.message)
+    }
+}
+
+export const login = async (auth, {navigate, dispatch}) => {
+    try {let _
+        let {data} = await axios.post('http://localhost:4000/api/auth/login',  auth)
+
+        dispatch({type: LOGIN, user: data})
+
         navigate('/')
     } catch (e) {
         navigate('/login-error')
         console.log(e.message)
     }
 }
-
-export const login = async (auth,{navigate,dispatch}) => {
+export const logout = async ({navigate, dispatch}) => {
     try {
-        let {data} = await axios.post('http://localhost:4000/api/auth/login',auth)
-        await axios.get(`http://localhost:4000/api/session/set/user/${data._id.toString()}`)
-        dispatch({type:LOGIN, user:data})
-
-        navigate('/')
+        let {status} = await axios.post('http://localhost:4000/api/auth/logout', {})
+        dispatch({type: LOGOUT, user: null})
     } catch (e) {
-        navigate('/login-error')
         console.log(e.message)
     }
 }
-export const logout = async ({navigate,dispatch}) => {
+
+export const updateProfile = async (user, {navigate, dispatch}) => {
     try {
-        let {status } = await axios.post('http://localhost:4000/api/auth/logout',{})
-        dispatch({type:LOGOUT, user:{_id:null}})
-        navigate('/logged-out')
+        let {data} = await axios.put(`http://localhost:4000/api/users/${user._id}`, user)
+        dispatch({type: UPDATE, user: data})
+        navigate('/profile')
     } catch (e) {
-        navigate('/login-error')
+        navigate('/error')
         console.log(e.message)
     }
 }
