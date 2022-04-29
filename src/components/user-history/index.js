e2806464a8a79d4b190fa989407ad20190df9cb8
donovanel <donovan.el@northeastern.edu>
2022-04-29 10:05:32 -0400

    redux stuff wip

--- src/components/user-history/index.js
+++ src/components/user-history/index.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, {useEffect} from "react";
 
 import history from '../data/reviewRatingCombo.json';
 
 import LikeItem from "./like-item";
 import DislikeItem from "./dislike-item";
+import {useDispatch, useSelector} from "react-redux";
+import axios from "axios";
 
 const UserHistory = () => {
+    const dispatch = useDispatch();
+    const ratings = useSelector(x=>x.ratings);
+    const user = useSelector(x=>x.currentUser);
+    const getRatingsByUser = async (uid, dispatch) => {
+        const {data:ratings} = await axios.get(`http://localhost:4000/api/users/${uid}/ratings?user=${uid}`)
 
+    }
+    useEffect(()=>{
+
+    },[])
     return (
         <>
             <ul>
