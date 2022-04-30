dcd929f3e78c42222b8a522cdd670ad8ea395eb4
Siddarth Sathyanarayanan <siddarth7090@gmail.com>
2022-04-30 17:19:18 -0400

    [feat]: formatted date

--- src/components/movie-page/index.js
+++ src/components/movie-page/index.js
@@ -6,19 +6,6 @@ import { getImage } from "../../util/constant";
 
 const MoviePage = (
   {
-    // movie = {
-    //   tmdb_id: 550,
-    //   title: "Fight Club",
-    //   homepage: "http://www.foxmovies.com/movies/fight-club",
-    //   release_date: "1999-10-15",
-    //   overview:
-    //     "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground fight clubs forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
-    //   poster_path: "/images/fight-club.jpg",
-    //   genre_ids: [28, 12, 878],
-    //   vote_average: 8.4,
-    //   _id: "62643e1edf7769194cc3a19d",
-    //   __v: 0,
-    // },
   }
 ) => {
   const [modalShow, setModalShow] = React.useState(false);
